b6ca3e2027c02fe54ffc5ae4b6df7bb323c14a06
matviy <stangersco@gmail.com>
2024-11-27 00:21:46 +0200

    Add function: open, close socket

--- client.cpp
+++ client.cpp
@@ -1,2 +1,47 @@
 #include "client.h"
 
+
+
+Client::Client()
+    : QObject()
+{
+    this->connection      = NULL;
+}
+
+
+bool Client::OpenSocket(const char *ip, int port)
+{
+    WSAData wsaData;
+    WORD DLLVersion = MAKEWORD(2, 1);
+
+    if (WSAStartup(DLLVersion, &wsaData) != 0)
+    {
+        return false;
+    }
+
+
+    SOCKADDR_IN addr;
+    addr.sin_addr.s_addr = inet_addr(ip);
+    addr.sin_port = htons(port);
+    addr.sin_family = AF_INET;
+
+    connection = socket(AF_INET, SOCK_STREAM, 0);
+
+    int code = ::connect(connection, (SOCKADDR*)&addr, sizeof(addr));
+    if (code)
+    {
+        closesocket(connection);
+        connection = NULL;
+        return false;
+    }
+    return true;
+}
+void Client::CloseSocket()
+{
+    if (connection)
+    {
+        shutdown(connection, SD_BOTH);
+        closesocket(connection);
+        connection = NULL;
+    }
+}
--- client.h
+++ client.h
@@ -4,16 +4,27 @@
 #include "datalib.h"
 #include <windows.h>
 
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
 #include <QObject>
 #include <QString>
+
+#define MAILSLOT_SERVER_NAME "\\\\.\\Mailslot\\Server"
+#define LOCAL_IP_ADDRESS "127.0.0.1"
+#define DEFAULT_PORT 1111
 
 class Client : public QObject
 {
     Q_OBJECT
 
+    SOCKET connection;
+
 public:
     Client();
 
+    bool OpenSocket(const char *ip = LOCAL_IP_ADDRESS, int port = DEFAULT_PORT);
+    void CloseSocket();
 };
 
 #endif // CLIENT_H
